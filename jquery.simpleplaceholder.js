33aa666f79aa7af01a31c5f161a3f794bed5449e
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-02-28 17:38:39 +0100

    some more fixes in code layout

--- jquery.simpleplaceholder.js
+++ jquery.simpleplaceholder.js
@@ -6,28 +6,28 @@ Simple Placeholder
 
 (function($) {
 	$.simplePlaceholder = {
-	placeholder_class: null,
+		placeholder_class: null,
 
-	hide_placeholder: function(){
-		var $this = $(this);
-		if($this.val() == $this.attr('placeholder')){
-			$this.val("").removeClass($.simplePlaceholder.placeholder_class);
-		}
-	},
-
-	show_placeholder: function(){
-		var $this = $(this);
-		if($this.val() == ""){
-			$this.val($this.attr('placeholder')).addClass($.simplePlaceholder.placeholder_class);
-		}
-	},
-
-	prevent_placeholder_submit: function(){
-		$(this).find(".simple-placeholder").each(function(e){
+		hide_placeholder: function(){
 			var $this = $(this);
 			if($this.val() == $this.attr('placeholder')){
-				$this.val('');
+				$this.val("").removeClass($.simplePlaceholder.placeholder_class);
 			}
+		},
+
+		show_placeholder: function(){
+			var $this = $(this);
+			if($this.val() == ""){
+				$this.val($this.attr('placeholder')).addClass($.simplePlaceholder.placeholder_class);
+			}
+		},
+
+		prevent_placeholder_submit: function(){
+			$(this).find(".simple-placeholder").each(function(e){
+				var $this = $(this);
+				if($this.val() == $this.attr('placeholder')){
+					$this.val('');
+				}
 			});
 			return true;
 		}
@@ -53,6 +53,6 @@ Simple Placeholder
 		}
 
 		return this;
-	}
+	};
 
 })(jQuery);
